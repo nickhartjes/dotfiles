831ecd87c0acaf670cf035ceea2ad020dc1a17da
Nick Hartjes <info@nickhartjes.nl>
2021-05-12 18:39:59 +0200

    First start of awesome

--- .config/awesome/configuration/apps.lua
+++ .config/awesome/configuration/apps.lua
@@ -1,6 +1,6 @@
 local filesystem = require('gears.filesystem')
 
--- Thanks to jo148 on github for making rofi dpi aware!
+-- -- Thanks to jo148 on github for making rofi dpi aware!
 local with_dpi = require('beautiful').xresources.apply_dpi
 local get_dpi = require('beautiful').xresources.get_dpi
 local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. with_dpi(400) .. ' -show drun -theme ' .. filesystem.get_configuration_dir() .. '/configuration/rofi.rasi -run-command "/bin/bash -c -i \'shopt -s expand_aliases; {cmd}\'"'
@@ -10,18 +10,18 @@ return {
   default = {
     terminal = 'alacritty',
     rofi = rofi_command,
-    -- lock = 'i3lock-fancy',
+    lock = 'i3lock-fancy',
     browser = 'firefox',
-    editor = 'code', -- gui text editor
-    game = rofi_command,
+    editor = 'vim', -- gui text editor
     files = 'nautilus'
   },
   -- List of apps to start once on start-up
   run_on_start_up = {
     'picom --config ' .. filesystem.get_configuration_dir() .. '/configuration/picom.conf',
-    -- 'nm-applet --indicator', -- wifi
-    -- 'pnmixer', -- shows an audiocontrol applet in systray when installed.
+    'nm-applet --indicator', -- wifi
+    'pnmixer', -- shows an audiocontrol applet in systray when installed.
     'feh --randomize --bg-fill ~/.wallpapers/*',
+    'blueman-applet',
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
     '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
---
+++ .config/awesome/configuration/client/buttons.lua
@@ -0,0 +1,17 @@
+local awful = require('awful')
+
+local modkey = require('configuration.keys.mod').modKey
+
+return awful.util.table.join(
+  awful.button({ }, 1, function (c)
+      c:emit_signal("request::activate", "mouse_click", {raise = true})
+  end),
+  awful.button({ modkey }, 1, function (c)
+      c:emit_signal("request::activate", "mouse_click", {raise = true})
+      awful.mouse.client.move(c)
+  end),
+  awful.button({ modkey }, 3, function (c)
+      c:emit_signal("request::activate", "mouse_click", {raise = true})
+      awful.mouse.client.resize(c)
+  end)
+)

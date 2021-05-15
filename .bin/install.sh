5116645f6fa03dca34c2528c44ec5cf34c2b65c6
Nick Hartjes <info@nickhartjes.nl>
2021-05-15 13:47:19 +0200

    Update install.sh
    Added submodules

--- .bin/install.sh
+++ .bin/install.sh
@@ -11,4 +11,5 @@ if [ $? = 0 ]; then
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 config checkout
+config submodule update
 config config status.showUntrackedFiles no

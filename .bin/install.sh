6e9348c0f8ba8f5c66a8c23d14321ddfc6197272
Nick Hartjes <info@nickhartjes.nl>
2021-06-05 09:01:21 +0200

    Update install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,4 +1,4 @@
-git clone --bare git@github.com:nickhartjes/dotfiles.git $HOME/.dotfiles
+git clone --recurse-submodules --bare git@github.com:nickhartjes/dotfiles.git $HOME/.dotfiles
 function config {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }

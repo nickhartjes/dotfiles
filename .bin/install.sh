63cf8f0db2764415b8fa93d672453f1fce951e8f
Nick Hartjes <info@nickhartjes.nl>
2021-09-06 11:35:02 +0200

    Added own photo's

--- .bin/install.sh
+++ .bin/install.sh
@@ -3,3 +3,4 @@ git clone --recurse-submodules git@github.com:nickhartjes/dotfiles.git $HOME/.do
 rsync -a --delete  $HOME/.dotfiles/.config/awesome  $HOME/.config/awesome
 rsync -a --delete  $HOME/.dotfiles/.config/.oh-my-zsh  $HOME/.config/.oh-my-zsh
 rsync -a --delete  $HOME/.dotfiles/.config/alacritty  $HOME/.config/alacritty
+rsync -a --delete  $HOME/.dotfiles/.wallpapers  $HOME/.wallpapers

a4156aee9aac9902ed3dce6682277a0001496c8a
Nick Hartjes <info@nickhartjes.nl>
2021-09-07 11:09:51 +0200

    Added chromium to path

--- install/sync.sh
+++ install/sync.sh
@@ -1,13 +1,17 @@
-rsync -a --delete  $HOME/.dotfiles/.config/awesome  $HOME/.config
-rsync -a --delete  $HOME/.dotfiles/.config/awesome-personal  $HOME/.config
-rsync -a --delete  $HOME/.dotfiles/.config/.oh-my-zsh  $HOME/.config
-rsync -a --delete  $HOME/.dotfiles/.config/alacritty  $HOME/.config
+rsync -a --delete  $HOME/.dotfiles/.config/awesome                      $HOME/.config
+rsync -a --delete  $HOME/.dotfiles/.config/awesome-personal             $HOME/.config
+rsync -a --delete  $HOME/.dotfiles/.config/.oh-my-zsh                   $HOME/.config
+rsync -a --delete  $HOME/.dotfiles/.config/alacritty                    $HOME/.config
 
-# Copy awesome settings
-cp -r ~/.config/awesome-personal/rc.lua ~/.config/awesome/rc.lua
-cp -r ~/.config/awesome-personal/picom.conf ~/.config/awesome/picom.conf
-cp -r ~/.config/awesome-personal/autostart.sh ~/.config/awesome/autostart.sh
-cp -r ~/.config/awesome-personal/themes/dremora/theme-personal.lua ~/.config/awesome/themes/dremora/theme-personal.lua
+# Copy awesome settings 
+cp -r $HOME/.config/awesome-personal/rc.lua                             $HOME/.config/awesome/rc.lua
+cp -r $HOME/.config/awesome-personal/picom.conf                         $HOME/.config/awesome/picom.conf
+cp -r $HOME/.config/awesome-personal/autostart.sh                       $HOME/.config/awesome/autostart.sh
+cp -r $HOME/.config/awesome-personal/themes/dremora/theme-personal.lua  $HOME/.config/awesome/themes/dremora/theme-personal.lua
 
 # Copy wallpapers
-rsync -a --delete  $HOME/.dotfiles/.wallpapers  $HOME/
+rsync -a --delete  $HOME/.dotfiles/.wallpapers                          $HOME/
+
+
+# Copy .zshrc config
+cp -r $HOME/.dotfiles/.zshrc-personal                                   $HOME/.zshrc-personal

1245d72f7bc9833910788cfac56c1c70c9462f06
Nick Hartjes <info@nickhartjes.nl>
2021-09-17 08:10:17 +0200

    Changed xrandr config

--- .config/awesome-personal/autostart.sh
+++ .config/awesome-personal/autostart.sh
@@ -14,16 +14,16 @@ run xfce4-power-manager
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 run numlockx on
 run volumeicon
-run ulauncher
 run flameshot
 
 run feh --bg-fill --randomize ~/.wallpapers
 
 if [ "$HOSTNAME" = "arco-laptop" ]; then
     run bluebery-tray
+    xrandr --auto --output HDMI1 --mode 1920x1080 --left-of eDP1
 fi
 
 if [ "$HOSTNAME" = "arco-desktop" ]; then
     echo "Arco Desktop loaded"
-    xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 0x604 --rotate normal --output DisplayPort-1 --mode 2560x1440 --pos 2560x0 --rotate left --output DisplayPort-2 --off --output HDMI-A-0 --off
+    xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 0x604 --rotate normal --output DisplayPort-1 --mode 2560x1440 --pos 2560x0 --rotate left
 fi
